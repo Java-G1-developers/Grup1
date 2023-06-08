26a07f11d54553fa09a125444f049fe5f353e010
JenyCris <jeny,metaute321@gmail.com>
2023-06-08 18:59:16 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/grupo1/bdh2/BdH2Application.java
@@ -0,0 +1,13 @@
+package com.grupo1.bdh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/bdh2/BdH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.bdh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
